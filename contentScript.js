c93338fcc30747343a12f7593c5f17b02c76a3e0
Simon Hoblik <shoblik@yahoo.com>
2018-08-22 11:23:39 -0700

    Sending and receiving response from contentScript.js to panel.js

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,19 @@
-console.log('fuck my ass');
-function fuckMyAss() {
-  console.log('fuckMyAss');
+document.querySelector('body').addEventListener('click', handleElementClicked, true);
+
+function handleElementClicked() {
+  console.log('click from the content script event listener');
+  document.querySelector('body').removeEventListener('click', handleElementClicked, true);
+
+  chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    console.log(response);
+  });
 }
+function handleResponse(message) {
+
+}
+function handleError(error) {
+
+}
+document.addEventListener("hello", function(data) {
+    chrome.runtime.sendMessage("test");
+});
--- panel.js
+++ panel.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-  $('.selectorBtn').on('click', makeSelection);
+    $(document).ready(function() {
+      $('.selectorBtn').on('click', makeSelection);
 
-});
-function makeSelection() {
-  let destination = $(this).attr('destination');
-  console.log(chrome);
-  chrome.tabs.executeScript(null, {
-      file: "contentScript.js",
-      runAt: "document_end"
-  });
-}
+    });
+    function makeSelection() {
+      let destination = $(this).attr('destination');
+      console.log(chrome);
+      chrome.tabs.executeScript({
+        file: "contentScript.js",
+        runAt: "document_end"
+      });
+    }
+    function handleMessage(request, sender, sendResponse) {
+      console.log("Message from the content script: " +
+        request.greeting);
+      sendResponse({response: "Response from background script from panel.jssss"});
+    }
+
+    chrome.runtime.onMessage.addListener(handleMessage);
