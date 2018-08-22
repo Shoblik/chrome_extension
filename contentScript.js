194248f796188fa93866b53fbc3db370917b98f1
Simon Hoblik <shoblik@yahoo.com>
2018-08-22 13:33:50 -0700

    walking up the selected element to find the elements path

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,53 @@
-document.querySelector('body').addEventListener('click', handleElementClicked, true);
+document.addEventListener('click', handleElementClicked, true);
 
 function handleElementClicked() {
-  console.log('click from the content script event listener');
-  document.querySelector('body').removeEventListener('click', handleElementClicked, true);
+  // prevent subsequent clicks by the user
+  document.removeEventListener('click', handleElementClicked, true);
 
-  chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    console.log(response);
+  // get the target elements three parents along with 2 class names
+  let targetEle = event.target;
+  let selector = null;
+
+  let eleTag = targetEle.localName;
+  if (targetEle.classList) {
+    eleTagClass = '.' + targetEle.classList[0];
+  } else {
+    eleTagClass = '';
+  }
+
+  let eleParent1 = targetEle.parentElement;
+  let eleParentTag1 = eleParent1.localName;
+  if (eleParent1.classList) {
+    eleParentClass1 = createClassArray(eleParent1.classList);
+  } else {
+    eleParentClass1 = '';
+  }
+
+  let eleParent2 = eleParent1.parentElement;
+  let eleParentTag2 = eleParent2.localName;
+  if (eleParent2.classList) {
+    eleParentClass2 = createClassArray(eleParent2.classList);
+  } else {
+    eleParentClass2 = '';
+  }
+
+  let eleParent3 = eleParent2.parentElement;
+  let eleParentTag3 = eleParent3.localName;
+  if (eleParent3.classList) {
+    eleParentClass3 = createClassArray(eleParent3.classList);
+  } else {
+    eleParentClass3 = '';
+  }
+  debugger;
+
+  chrome.runtime.sendMessage({data: "eleThatWasClicked"}, function(response) {
+    console.log(response + 'from contentScript.js');
   });
 }
-function handleResponse(message) {
-
+function createClassArray(obj) {
+  let tmp = '';
+  for (let i = 0; i < obj.length; i++) {
+    tmp += '.' + obj[i];
+  }
+  return tmp;
 }
-function handleError(error) {
-
-}
-document.addEventListener("hello", function(data) {
-    chrome.runtime.sendMessage("test");
-});
--- panel.js
+++ panel.js
@@ -4,16 +4,16 @@
     });
     function makeSelection() {
       let destination = $(this).attr('destination');
-      console.log(chrome);
       chrome.tabs.executeScript({
         file: "contentScript.js",
         runAt: "document_end"
       });
     }
     function handleMessage(request, sender, sendResponse) {
-      console.log("Message from the content script: " +
-        request.greeting);
-      sendResponse({response: "Response from background script from panel.jssss"});
+      console.log('message from the content script');
+      console.log(request);
+
+      sendResponse("Response from panel.js - received the payload");
     }
 
     chrome.runtime.onMessage.addListener(handleMessage);
